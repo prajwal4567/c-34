30bd572949dffa5528cca1796de8358e109b1b6f
prajwal4567 <82152156+prajwal4567@users.noreply.github.com>
2021-05-31 17:03:34 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,43 @@
+var ball;
+var database,position;
+function setup(){
+    database=firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballref=database.ref('ball/position');
+    ballref.on("value",readposition,showerror);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readposition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showerror(){
+    console.log("error");
+}
